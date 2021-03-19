784f070f5b5e1c87ec857edac12484f711e2a849
hankpeeples <hgpeeples48@gmail.com>
2021-03-19 00:10:51 -0500

    Added some permission based validation

--- commands/youtube.js
+++ commands/youtube.js
@@ -3,6 +3,14 @@ module.exports = {
     description: "sends youtube link",
     excecute(msg, args) {
         // code for command
-        msg.channel.send('https://youtube.com');
+        if (msg.member.roles.cache.has('822331403323965471')) {
+            msg.channel.send('https://youtube.com');
+            // remove role from user
+            msg.member.roles.remove('822331403323965471').catch(console.error);
+        } else {
+            msg.reply('I see you do not have the correct permissions, let me change that :) now try!');
+            // add needed role to user
+            msg.member.roles.add('822331403323965471').catch(console.error);
+        }
     }
 }
