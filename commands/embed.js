a6205d9a80a2288372b9f554fa1cdb5045c617b5
hankpeeples <hgpeeples48@gmail.com>
2021-03-21 18:46:33 -0500

    Adjusted formatting

--- commands/embed.js
+++ commands/embed.js
@@ -1,21 +1,20 @@
 module.exports = {
-	name: 'embed',
-	description: "Embeds!",
-	excecute(msg, args, Discord) {
-		const embed = new Discord.MessageEmbed()
-			.setColor('#304281')
-			.setTitle('Rules')
-			.setURL('https://github.com/hankpeeples')
-			.setDescription('Embed for server rules.')
-			.addFields({
-				name: 'Rule 1',
-				value: 'Be nice.'
-			}, {
-				name: 'Rule 2',
-				value: 'Don\'t be cunt'
-			});
+   name: 'embed',
+   description: "Embeds!",
+   excecute(msg, args, Discord) {
+      const embed = new Discord.MessageEmbed()
+         .setColor('#304281')
+         .setTitle('Rules')
+         .setURL('https://github.com/hankpeeples')
+         .setDescription('Embed for server rules.')
+         .addFields({
+            name: 'Rule 1',
+            value: 'Be nice.'
+         }, {
+            name: 'Rule 2',
+            value: 'Don\'t be cunt'
+         });
 
-		msg.channel.send(embed);
-	}
-
+      msg.channel.send(embed);
+   }
 }
