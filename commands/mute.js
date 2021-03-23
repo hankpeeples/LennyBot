da37273dd216d288497d742698923bb4336fac7a
hankpeeples <hgpeeples48@gmail.com>
2021-03-22 20:51:40 -0500

    Added permission validation

--- commands/mute.js
+++ commands/mute.js
@@ -2,36 +2,41 @@ const ms = require('ms');
 
 module.exports = {
    name: 'mute',
-   description: 'Essentially mute a server member',
+   description: 'Mute a server member',
    excecute(msg, args) {
       const target = msg.mentions.users.first();
-      // make sure target exists
-      if (target) {
-         let mainRole = msg.guild.roles.cache.find(role => role.name === 'test');
-         let muteRole = msg.guild.roles.cache.find(role => role.name === 'mute');
+      // check if user has permission to use command
+      if (msg.member.roles.cache.has('811142977673232385')) {
+         // make sure target exists
+         if (target) {
+            let mainRole = msg.guild.roles.cache.find(role => role.name === 'test');
+            let muteRole = msg.guild.roles.cache.find(role => role.name === 'mute');
 
-         // target info
-         let memberTarget = msg.guild.members.cache.get(target.id);
+            // target info
+            let memberTarget = msg.guild.members.cache.get(target.id);
 
-         if (!args[1]) {
-            // manual mute
+            if (!args[1]) {
+               // manual mute
+               memberTarget.roles.remove(mainRole.id);
+               memberTarget.roles.add(muteRole.id);
+               msg.channel.send(`<@${memberTarget.user.id}> has been muted.`);
+               return;
+            }
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
-            msg.channel.send(`<@${memberTarget.user.id}> has been muted.`);
-            return;
+            msg.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}.`);
+
+            // Function will crash bot if it doesn't get numerical input
+            setTimeout(function () {
+               memberTarget.roles.remove(muteRole.id);
+               memberTarget.roles.add(mainRole.id);
+            }, ms(args[1]));
+
+         } else {
+            msg.reply('Can\'t find that member!');
          }
-         memberTarget.roles.remove(mainRole.id);
-         memberTarget.roles.add(muteRole.id);
-         msg.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}.`);
-
-         // Function will crash bot if it doesn't get numerical input
-         setTimeout(function () {
-            memberTarget.roles.remove(muteRole.id);
-            memberTarget.roles.add(mainRole.id);
-         }, ms(args[1]));
-
       } else {
-         msg.reply('Can\'t find that member!');
+         msg.reply('You do not have permission to use this command!');
       }
    }
 }
--- commands/unmute.js
+++ commands/unmute.js
@@ -3,21 +3,26 @@ module.exports = {
    description: 'Unmutes a server member',
    excecute(msg, args) {
       const target = msg.mentions.users.first();
-      // make sure target exists
-      if (target) {
-         let mainRole = msg.guild.roles.cache.find(role => role.name === 'test');
-         let muteRole = msg.guild.roles.cache.find(role => role.name === 'mute');
+      // check if user has permission to use command
+      if (msg.member.roles.cache.has('811142977673232385')) {
+         // make sure target exists
+         if (target) {
+            let mainRole = msg.guild.roles.cache.find(role => role.name === 'test');
+            let muteRole = msg.guild.roles.cache.find(role => role.name === 'mute');
 
-         // target info
-         let memberTarget = msg.guild.members.cache.get(target.id);
+            // target info
+            let memberTarget = msg.guild.members.cache.get(target.id);
 
-         // manual unmute
-         memberTarget.roles.remove(muteRole.id);
-         memberTarget.roles.add(mainRole.id);
+            // manual unmute
+            memberTarget.roles.remove(muteRole.id);
+            memberTarget.roles.add(mainRole.id);
 
-         msg.channel.send(`<@${memberTarget.user.id}> has been unmuted.`);
+            msg.channel.send(`<@${memberTarget.user.id}> has been unmuted.`);
+         } else {
+            msg.reply('Can\'t find that member!');
+         }
       } else {
-         msg.reply('Can\'t find that member!');
+         msg.reply('You do not have permission to use this command!');
       }
    }
 }
