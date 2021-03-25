8a877934634e9b20dde09bd8aa35c180f79eb135
hankpeeples <hgpeeples48@gmail.com>
2021-03-24 22:00:14 -0500

    Small change

--- commands/oob.js
+++ commands/oob.js
@@ -3,8 +3,7 @@ module.exports = {
     aliases: ['oob'],
     description: "Swap vowels with 'oob'",
     async run(client, args, cmd, message) {
-        // Could use args but this seems easier
-        var oobStr = message.content.slice(4).trim();
-        message.reply(oobStr.replace(/[aeiou]/gi, 'oob'));
+        var oob_str = args.join(' ');
+        message.reply(oob_str.replace(/[aeiou]/gi, 'oob'));
     }
 }
