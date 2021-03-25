8983cdbf7ae00c1fd27a6275871dbeddb2bb2e0f
hankpeeples <hgpeeples48@gmail.com>
2021-03-24 21:25:01 -0500

    Added reddit meme command

---
+++ commands/meme.js
@@ -0,0 +1,29 @@
+const {
+    meme
+} = require('memejs');
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    name: 'meme',
+    aliases: ['meme pls'],
+    description: "Grab meme from reddit",
+    async run(client, args, cmd, message) {
+        message.channel.send('Getting your meme... :thought_balloon:');
+        const sub = 'terriblefacebookmemes';
+        meme(sub).then(meme => {
+            console.log(meme)
+            const memeEmbed = new MessageEmbed()
+                .setColor('#845ec2')
+                .setAuthor(`r/${meme.subreddit}`)
+                .setURL(meme.url)
+                .setTitle(meme.title)
+                .setImage(meme.url);
+            message.channel.send(memeEmbed);
+        }).catch(err => {
+            message.channel.send(':exclamation: Sometimes it fucks up, we all have our days...try again :exclamation:');
+            throw err;
+        });
+    }
+}
