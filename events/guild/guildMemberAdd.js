74005e4ab3645083c05a3c41d2bd86469a2c0fbc
hankpeeples <hgpeeples48@gmail.com>
2021-03-25 22:06:26 -0500

    Prefix fix

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,7 @@
-module.exports = (Discord, client, guildMember) => {
-    const prefix = '#';
+require('dotenv').config();
+const PREFIX = process.env.PREFIX;
 
+module.exports = (Discord, client, guildMember) => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'test');
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('706588663747969107').send(`Welcome <@${guildMember.user.id}>! For a list of my commands type **${prefix}help**!`);
