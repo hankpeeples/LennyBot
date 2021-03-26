17eec5a9526f373fe1d002519731e834e8e9edac
hankpeeples <hgpeeples48@gmail.com>
2021-03-26 18:20:58 -0500

    Added changing activity status every 4min

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,8 +2,25 @@ const memberCounter = require('../../counters/member_counter');
 
 module.exports = (Discord, client) => {
     console.log("\x1b[31m%s\x1b[0m", `\nLenny is online!`);
-    client.user.setActivity('with Lenny', {
-        type: "PLAYING"
-    });
+    setInterval(changeStatus, 240000); // change every 4 minutes
     memberCounter(client);
+
+    function changeStatus() {
+        console.log("\nChanging status!");
+        const status = [
+            "with your feelings",
+            "with perry",
+            "with griffen",
+            "with gabriel",
+            "with henry",
+            "with lenny",
+            "innapropriate things"
+        ];
+        let randomStatus = status[Math.floor(Math.random() * status.length)];
+        client.user.setActivity(randomStatus, {
+            type: "PLAYING"
+        }).catch(err => {
+            console.log(err, 'Failed to set activity!');
+        });
+    }
 }
