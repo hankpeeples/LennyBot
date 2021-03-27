244023c0cad1985d9ec6aea2e2dbe824351d18b2
hankpeeples <hgpeeples48@gmail.com>
2021-03-26 23:31:25 -0500

    Small fix

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -5,4 +5,5 @@ module.exports = (Discord, client, guildMember) => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'test');
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('706588663747969107').send(`Welcome <@${guildMember.user.id}>! For a list of my commands type **${PREFIX}help**!`);
+
 }
