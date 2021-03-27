3f15fbfed97d0976b11ea919343c489d691ee195
hankpeeples <hgpeeples48@gmail.com>
2021-03-27 00:04:29 -0500

    Added initial MongoDB connection

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const MONGODB_SRV = process.env.MONGODB_SRV;
 
 // Make collection for commands and events
 client.commands = new Discord.Collection();
@@ -10,6 +12,17 @@ client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
+});
+
+// DB connection
+mongoose.connect(MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Database connected!');
+}).catch(err => {
+    console.log(err);
 });
 
 client.login(TOKEN);
