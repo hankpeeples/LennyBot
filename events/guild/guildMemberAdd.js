fcc804797e63a816989f9bdeae3baa8ea1b98371
hankpeeples <hgpeeples48@gmail.com>
2021-03-26 23:00:09 -0500

    Welcome event fix

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -4,5 +4,5 @@ const PREFIX = process.env.PREFIX;
 module.exports = (Discord, client, guildMember) => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'test');
     guildMember.roles.add(welcomeRole);
-    guildMember.guild.channels.cache.get('706588663747969107').send(`Welcome <@${guildMember.user.id}>! For a list of my commands type **${prefix}help**!`);
+    guildMember.guild.channels.cache.get('706588663747969107').send(`Welcome <@${guildMember.user.id}>! For a list of my commands type **${PREFIX}help**!`);
 }
