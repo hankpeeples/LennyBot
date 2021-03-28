79afd0d5f9d5336a61bcf482f29f30549b0ef734
hankpeeples <hgpeeples48@gmail.com>
2021-03-28 18:19:27 -0500

    Fixed member counter

--- counters/member_counter.js
+++ counters/member_counter.js
@@ -1,11 +1,11 @@
 // counts number of server members every 5 minutes and adds the number to the voice channel name
 module.exports = async (client) => {
     // guild
-    const guild = client.guilds.cache.get('706588663747969104');
+    const guild = client.guilds.cache.get('623292331214372884');
     setInterval(() => {
         const memberCount = guild.memberCount;
         // get voice channel
-        const channel = guild.channels.cache.get('823757139523928094');
+        const channel = guild.channels.cache.get('825870943771164673');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating member count');
     }, 300000);
