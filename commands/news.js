389bb4a3c92e3824fbbdd54493670cf327bd4a2b
hankpeeples <hgpeeples48@gmail.com>
2021-03-28 13:35:03 -0500

    Updated embeds

--- commands/news.js
+++ commands/news.js
@@ -13,17 +13,29 @@ module.exports = {
                 return res.json();
             }).then(news => {
                 const info = news.news[0];
-                const current = new discord.MessageEmbed()
-                    .setColor('RED')
-                    .setAuthor(`Author: ${info.author}, Published on ${info.published.substr(0, 10)}`)
-                    .setTitle(`Top News`)
-                    .setURL(info.url)
-                    .setDescription(`**${info.title}**`)
-                    .addField('Content 📰', `${info.description} | **Click link at the top for full article!**`)
-                    .setImage(info.image)
-                    .setTimestamp(new Date())
-                    .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
-                return message.channel.send(current);
+                // not the best way to do this but whatever
+                if (info.image === 'None') {
+                    const current = new discord.MessageEmbed()
+                        .setColor('RED')
+                        .setAuthor(`Author: ${info.author}, Published on ${info.published.substr(0, 10)}`)
+                        .setTitle(`Top News ℹ️ \n\n${info.title}`)
+                        .setDescription(`[**_Link to article_**](${info.url})`)
+                        .addField('Content 📰', `${info.description}`)
+                        .setTimestamp(new Date())
+                        .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
+                    return message.channel.send(current);
+                } else {
+                    const current = new discord.MessageEmbed()
+                        .setColor('RED')
+                        .setAuthor(`Author: ${info.author}, Published on ${info.published.substr(0, 10)}`)
+                        .setTitle(`Top News ℹ️ \n\n${info.title}`)
+                        .setDescription(`[**_Link to article_**](${info.url})`)
+                        .addField('Content 📰', `${info.description}`)
+                        .setImage(info.image)
+                        .setTimestamp(new Date())
+                        .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
+                    return message.channel.send(current);
+                }
             }).catch(err => {
                 message.channel.send(':exclamation: Error receiving news!');
                 throw err;
@@ -35,18 +47,28 @@ module.exports = {
                 return res.json();
             }).then(news => {
                 const info = news.news[0];
-                console.log(info);
-                const search = new discord.MessageEmbed()
-                    .setColor('RED')
-                    .setAuthor(`Author: ${info.author}, Published on ${info.published}`)
-                    .setTitle(`Top News result for "${keyword}"`)
-                    .setURL(info.url)
-                    .setDescription(`**${info.title}**`)
-                    .addField('Content 📰', `${info.description} | **Click link at the top for full article!**`)
-                    .setImage(info.image)
-                    .setTimestamp(new Date())
-                    .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
-                return message.channel.send(search);
+                if (info.image === 'None') {
+                    const search = new discord.MessageEmbed()
+                        .setColor('RED')
+                        .setAuthor(`Author: ${info.author}, Published on ${info.published.substr(0, 10)}`)
+                        .setTitle(`Top News result for "${keyword}" ℹ️ \n\n${info.title}`)
+                        .setDescription(`[**_Link to article_**](${info.url})`)
+                        .addField('Content 📰', `${info.description}`)
+                        .setTimestamp(new Date())
+                        .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
+                    return message.channel.send(search);
+                } else {
+                    const search = new discord.MessageEmbed()
+                        .setColor('RED')
+                        .setAuthor(`Author: ${info.author}, Published on ${info.published.substr(0, 10)}`)
+                        .setTitle(`Top News result for "${keyword}" ℹ️ \n\n${info.title}`)
+                        .setDescription(`[**_Link to article_**](${info.url})`)
+                        .addField('Content 📰', `${info.description}`)
+                        .setImage(info.image)
+                        .setTimestamp(new Date())
+                        .setFooter(`News requested by ${message.author.username}`, message.author.avatarURL());
+                    return message.channel.send(search);
+                }
             }).catch(err => {
                 message.channel.send(':exclamation: Error receiving news!');
                 throw err;
