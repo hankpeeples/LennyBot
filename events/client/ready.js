c421a0e124c138c4a53ba6537f4d562f4e49a3ec
hankpeeples <hgpeeples48@gmail.com>
2021-03-28 19:00:25 -0500

    Changed c.log

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,7 @@
 const memberCounter = require('../../counters/member_counter');
 
 module.exports = (Discord, client) => {
-    console.log("\x1b[31m%s\x1b[0m", `\nLenny is online!`);
+    console.log(`\nLenny is online!`);
     setInterval(changeStatus, 240000); // change every 4 minutes
     memberCounter(client);
 
